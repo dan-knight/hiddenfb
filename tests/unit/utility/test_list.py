95b97e48e8a424749ae4860ca0b2d5fbaf9c7a94
dan-knight <dan@danknight.dev>
2024-02-04 23:50:12 -0800

    Utility function to flatten lists

---
+++ tests/unit/utility/test_list.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from hiddenfb.utility.list import flatten
+
+
+def test__flatten__handles_list_of_single_elements():
+    values: List[int] = [1, 2, 3, 4]
+
+    result: List[int] = flatten(values)
+
+    assert result == values
+
+
+def test__flatten__handles_list_of_lists():
+    values: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    slice_index: int = 3
+    input_values: List[List[int]] = [values[:slice_index], values[slice_index:]]
+
+    result: List[int] = flatten(input_values)
+
+    assert result == values
+
+def test__flatten__handles_mixed_elements_and_lists():
+    values: List[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    slice_index: int = 3
+    input_values: List[int | List[int]] = values[:slice_index] + [values[slice_index:]]
+
+    result: List[int] = flatten(input_values)
+
+    assert result == values
