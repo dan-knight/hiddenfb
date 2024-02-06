6f4bf9a4e38885886297ab29aa64059dd590a4b7
dan-knight <dan@danknight.dev>
2024-02-05 19:06:37 -0800

    Overloads for generic validation schemas

--- hiddenfb/utility/schema/__init__.py
+++ hiddenfb/utility/schema/__init__.py
@@ -1,4 +1,16 @@
-from typing import TYPE_CHECKING, Any, Generic, Iterable, Mapping, Optional, TypeVar
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Generic,
+    Iterable,
+    List,
+    Literal,
+    Mapping,
+    Optional,
+    TypeVar,
+    cast,
+    overload,
+)
 
 from marshmallow import Schema, types
 
@@ -7,14 +19,60 @@ T = TypeVar("T")
 
 class GenericSchema(Schema, Generic[T]):
     if TYPE_CHECKING:
+
+        @overload
         def load(
             self,
-            data: Mapping[str, Any] | Iterable[Mapping[str, Any]],
+            data: Iterable[Mapping[str, Any]],
             *,
-            many: Optional[bool] = None,
+            many: Literal[True],
             partial: types.StrSequenceOrSet | bool | None = None,
-            unknown: Optional[str] = None
-        ) -> T:
-            return super().load(  # pyright: ignore[reportGeneralTypeIssues, reportUnknownVariableType]
-                data, many=many, partial=partial, unknown=unknown
-            )
+            unknown: Optional[str] = None,
+        ) -> List[T]: ...
+
+        @overload
+        def load(
+            self,
+            data: Iterable[Mapping[str, Any]],
+            *,
+            many: None = None,
+            partial: types.StrSequenceOrSet | bool | None = None,
+            unknown: Optional[str] = None,
+        ) -> List[T]: ...
+
+        @overload
+        def load(
+            self,
+            data: Mapping[str, Any],
+            *,
+            many: Literal[False],
+            partial: types.StrSequenceOrSet | bool | None = None,
+            unknown: Optional[str] = None,
+        ) -> T: ...
+
+        @overload
+        def load(
+            self,
+            data: Mapping[str, Any],
+            *,
+            many: None = None,
+            partial: types.StrSequenceOrSet | bool | None = None,
+            unknown: Optional[str] = None,
+        ) -> T: ...
+
+    def load(
+        self,
+        data: Mapping[str, Any] | Iterable[Mapping[str, Any]],
+        *,
+        many: bool | None = None,
+        partial: types.StrSequenceOrSet | bool | None = None,
+        unknown: Optional[str] = None,
+    ):
+        if many is None:
+            many = not isinstance(data, dict)
+
+        result = super().load(  # pyright: ignore[reportGeneralTypeIssues, reportUnknownVariableType]
+            data, many=many, partial=partial, unknown=unknown
+        )
+
+        return cast(T if many is True else List[T], result)
