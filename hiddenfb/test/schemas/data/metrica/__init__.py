56504a9ecf1386f901a6d6edacffd1097844f65a
dan-knight <dan@danknight.dev>
2024-02-08 09:50:04 -0800

    Test utilities for Metrica event data

---
+++ hiddenfb/test/schemas/data/metrica/__init__.py
@@ -0,0 +1,8 @@
+from typing import Literal
+from hiddenfb.test import TestUtility
+
+
+class MetricaTestUtility(TestUtility):
+    @property
+    def _default_value(self) -> Literal["NaN"]:
+        return "NaN"
