2bca16110ba5d6f012534e73f21c6ce65889db3b
dan-knight <dan@danknight.dev>
2024-02-13 11:01:08 -0800

    Fix code style

--- hiddenfb/database/repository/__init__.py
+++ hiddenfb/database/repository/__init__.py
@@ -3,19 +3,16 @@ from typing import Generic, TypeVar
 
 from hiddenfb.utility.file_loader import FileLoader
 
-
 ModelType = TypeVar("ModelType")
 StoreType = TypeVar("StoreType")
 
 
 class Repository(Generic[ModelType, StoreType], ABC):
     @abstractmethod
-    def _to_model(self, stored: StoreType) -> ModelType:
-        ...
-    
+    def _to_model(self, stored: StoreType) -> ModelType: ...
+
     @abstractmethod
-    def _from_model(self, model: ModelType) -> StoreType:
-        ...
+    def _from_model(self, model: ModelType) -> StoreType: ...
 
 
 class FileRepository(Repository[ModelType, StoreType]):
