1b6f0c6baa12bdab11219cf3a286bd5f3ef9d475
dan-knight <dan@danknight.dev>
2024-02-22 09:23:46 -0800

    Generic player moment to contain event type

--- hiddenfb/domain/moment/player/__init__.py
+++ hiddenfb/domain/moment/player/__init__.py
@@ -1,12 +1,16 @@
 from dataclasses import dataclass
+from typing import Generic, TypeVar
 
 from hiddenfb.domain.moment.match import MatchMoment
 from hiddenfb.domain.moment.player.action import PlayerAction
 from hiddenfb.domain.player import Player
 
 
+ActionType = TypeVar("ActionType", bound=PlayerAction | None)
+
+
 @dataclass
-class PlayerMoment:
+class PlayerMoment(Generic[ActionType]):
     player: Player
     match_moment: MatchMoment
-    action: PlayerAction
+    action: ActionType
