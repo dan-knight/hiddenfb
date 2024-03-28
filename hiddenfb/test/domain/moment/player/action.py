d7dcfafe1a70c51c383e4293b3c27000cb093ffe
dan-knight <dan@danknight.dev>
2024-03-28 11:26:17 -0700

    Use coordinate utilities in other test utilities

--- hiddenfb/test/domain/moment/player/action.py
+++ hiddenfb/test/domain/moment/player/action.py
@@ -6,8 +6,11 @@ from hiddenfb.domain.moment.player.action.shot import Shot, ShotResult
 
 ActionType = TypeVar("ActionType", bound=PlayerAction)
 
+
 class PlayerActionTestUtility:
-    def create_player_action(self, action_type: Type[ActionType] = Shot) -> PlayerAction:
+    def create_player_action(
+        self, action_type: Type[ActionType] = Shot
+    ) -> PlayerAction:
         if action_type == Shot:
             return Shot(result=ShotResult.GOAL)
         else:
