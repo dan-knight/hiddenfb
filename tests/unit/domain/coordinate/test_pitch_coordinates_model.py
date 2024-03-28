6e588014035f9a7e0280dab82e4757d776cfbba0
dan-knight <dan@danknight.dev>
2024-03-28 10:21:51 -0700

    Update coordinates model unit tests

--- tests/unit/domain/coordinate/test_pitch_coordinates_model.py
+++ tests/unit/domain/coordinate/test_pitch_coordinates_model.py
@@ -4,14 +4,14 @@ from hiddenfb.domain.coordinate.pitch import PitchCoordinates
 from hiddenfb.utility.exception.number import NegativeNumberError
 
 
-def test__pitch_coordinate__errors_with_negative_x():
+def test__pitch_coordinates__error_with_negative_x():
     negative_x: float = -10.5
 
     with pytest.raises(NegativeNumberError, match=str(negative_x)):
         PitchCoordinates(x=negative_x, y=10)
 
 
-def test__pitch_coordinate__errors_with_negative_y():
+def test__pitch_coordinates__error_with_negative_y():
     negative_y: float = -10.5
 
     with pytest.raises(NegativeNumberError, match=str(negative_y)):
