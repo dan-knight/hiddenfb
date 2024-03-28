de3a0fb343231e1e0f14e9a6748f968dcba7eb4e
dan-knight <dan@danknight.dev>
2024-03-28 11:10:07 -0700

    Variable name update for clarity

---
+++ hiddenfb/mappings/domain/coordinates/__init__.py
@@ -0,0 +1,16 @@
+from hiddenfb.domain.coordinate.pitch import PitchCoordinates
+from hiddenfb.schemas.data.wyscout.event import WyscoutEvent
+from hiddenfb.schemas.data.wyscout.event.position import WyscoutEventPosition
+
+
+class CoordinatesMapper:
+    def from_wyscout_event(self, event: WyscoutEvent) -> PitchCoordinates:
+        try:
+            start_position: WyscoutEventPosition = event.positions[0]
+        except IndexError:
+            raise ValueError("No positions found in Wyscout event.")
+        
+        return PitchCoordinates(
+            x=start_position.x,
+            y=start_position.y,
+        )
