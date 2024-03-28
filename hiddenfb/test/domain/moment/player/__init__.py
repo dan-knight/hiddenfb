b9fe998dd752acf4c3d6ca57906fac2c3b6e65e4
dan-knight <dan@danknight.dev>
2024-03-28 09:05:31 -0700

    Update test utilities

--- hiddenfb/test/domain/moment/player/__init__.py
+++ hiddenfb/test/domain/moment/player/__init__.py
@@ -1,19 +1,41 @@
-from typing import TypeVar
+from typing import Any, Dict
+from hiddenfb.domain.moment.match import MatchMoment
 
 from hiddenfb.domain.moment.player import PlayerMoment
 from hiddenfb.domain.moment.player.action import PlayerAction
 from hiddenfb.domain.player import Player
+from hiddenfb.test.domain.match import MatchTestUtility
+from hiddenfb.test.domain.moment.match import MatchMomentTestUtility
+from hiddenfb.test.domain.moment.player.action import PlayerActionTestUtility
 from hiddenfb.test.domain.player import PlayerTestUtility
-
-ActionType = TypeVar("ActionType", bound=PlayerAction | None)
 
 
 class PlayerMomentTestUtility:
     def create_player_moment(
-        self, player: Player | None = None, action: ActionType = None
-    ) -> PlayerMoment[ActionType]:
+        self, player: Player | None = None, match_moment: MatchMoment | None = None, player_action: PlayerAction | None = None
+    ) -> PlayerMoment[PlayerAction]:
         player_utility = PlayerTestUtility()
+        match_utility = MatchTestUtility()
+        match_moment_utility = MatchMomentTestUtility(match_utility=match_utility)
+        player_action_utility = PlayerActionTestUtility()
+
         if player is None:
             player = player_utility.create_player()
+        
+        if match_moment is None:
+            match_moment = match_moment_utility.create_match_moment()
 
-        raise NotImplementedError()
+        if player_action is None:
+            player_action = player_action_utility.create_player_action()
+
+        return PlayerMoment(
+            player=player,
+            match_moment=match_moment,
+            action=player_action
+        )
+    
+    def assert_equal(self, a: PlayerMoment[PlayerAction], b: PlayerMoment[PlayerAction]):
+        assert self.to_test_dict(a) == self.to_test_dict(b)
+
+    def to_test_dict(self, x: PlayerMoment[PlayerAction]) -> Dict[str, Any]:
+        return x.__dict__
