6593e9b1044377d9f196d13fb8e869ce6128dfdf
dan-knight <dan@danknight.dev>
2024-03-28 23:37:11 -0700

    Coordinate domain model mappings

--- hiddenfb/mappings/domain/coordinates/__init__.py
+++ hiddenfb/mappings/domain/coordinates/__init__.py
@@ -1,4 +1,5 @@
 from hiddenfb.domain.coordinate.pitch import PitchCoordinates
+from hiddenfb.schemas.data.metrica.event import MetricaEvent
 from hiddenfb.schemas.data.wyscout.event import WyscoutEvent
 from hiddenfb.schemas.data.wyscout.event.position import WyscoutEventPosition
 
@@ -13,4 +14,19 @@ class CoordinatesMapper:
         return PitchCoordinates(
             x=start_position.x,
             y=start_position.y,
+        )
+
+    def from_metrica_event(self, event: MetricaEvent) -> PitchCoordinates:
+        start_x: float
+        start_y: float
+        if event.event_subtype == "KICK_OFF":
+            start_x, start_y = 0.5, 0.5
+        elif event.start_x is None or event.start_y is None:
+            raise ValueError("Missing event coordinates")
+        else:
+            start_x, start_y = event.start_x, event.start_y
+
+        return PitchCoordinates(
+            x=start_x,
+            y=start_y,
         )
