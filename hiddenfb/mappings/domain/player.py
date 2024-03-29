064c81c8d898fe73983058b9b1d44408bbecd237
dan-knight <dan@danknight.dev>
2024-03-28 23:15:48 -0700

    Metrica event player mapping

--- hiddenfb/mappings/domain/player.py
+++ hiddenfb/mappings/domain/player.py
@@ -1,7 +1,19 @@
+import re
+
 from hiddenfb.domain.player import Player
+from hiddenfb.schemas.data.metrica.event import MetricaEvent
 from hiddenfb.schemas.data.wyscout.event import WyscoutEvent
 
 
 class PlayerMapper:
     def from_wyscout_event(self, event: WyscoutEvent) -> Player:
         return Player(player_id=event.player_id)
+
+    def from_metrica_event(self, event: MetricaEvent) -> Player:
+        return Player(player_id=self._parse_metrica_player_id(event.player_from))
+
+    def _parse_metrica_player_id(self, player_name: str) -> int:
+        number_matches: list[str] = re.findall(r"\d+", player_name)
+        if len(number_matches) == 0:
+            raise ValueError(f'No player ID found in "{player_name}"')
+        return int(number_matches[0])
