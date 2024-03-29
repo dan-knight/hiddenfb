c8546622e0d747ed657223fbdfb6ad57e5487c71
dan-knight <dan@danknight.dev>
2024-03-28 23:42:30 -0700

    Metrica event match moment mapper

--- hiddenfb/mappings/domain/moment/match.py
+++ hiddenfb/mappings/domain/moment/match.py
@@ -1,5 +1,6 @@
 from hiddenfb.domain.moment.match import MatchMoment
 from hiddenfb.mappings.domain.match import MatchMapper
+from hiddenfb.schemas.data.metrica.event import MetricaEvent
 from hiddenfb.schemas.data.wyscout.event import WyscoutEvent
 
 
@@ -10,4 +11,10 @@ class MatchMomentMapper:
     def from_wyscout_event(self, event: WyscoutEvent) -> MatchMoment:
         return MatchMoment(
             match=self._match_mapper.from_wyscout_event(event), frame=event.event_time
+        )
+
+    def from_metrica_event(self, event: MetricaEvent, match_id: int) -> MatchMoment:
+        return MatchMoment(
+            match=self._match_mapper.from_metrica_event(event, match_id=match_id),
+            frame=event.start_frame,
         )
