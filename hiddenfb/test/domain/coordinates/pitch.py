dbaad2099cf1b34729199009fcf0fbcdb06e3d4f
dan-knight <dan@danknight.dev>
2024-03-28 23:37:26 -0700

    Tests and utilities for coordinates and mappings

--- hiddenfb/test/domain/coordinates/pitch.py
+++ hiddenfb/test/domain/coordinates/pitch.py
@@ -1,3 +1,6 @@
+from typing import Any, Dict
+
+from hiddenfb.domain.coordinate import Coordinates
 from hiddenfb.domain.coordinate.pitch import PitchCoordinates
 from hiddenfb.test.domain.coordinates import CoordinatesTestUtility
 
@@ -5,3 +8,9 @@ from hiddenfb.test.domain.coordinates import CoordinatesTestUtility
 class PitchCoordinatesTestUtility(CoordinatesTestUtility):
     def create_coordinates(self, x: float = 1.4, y: float = 10.5) -> PitchCoordinates:
         return PitchCoordinates(x=x, y=y)
+
+    def assert_equal(self, a: Coordinates, b: Coordinates):
+        assert self.to_test_dict(a) == self.to_test_dict(b)
+
+    def to_test_dict(self, x: Coordinates) -> Dict[str, Any]:
+        return x.__dict__
