1d902205952444c1de884d1e187d1fb42ce470b9
dan-knight <dan@danknight.dev>
2024-03-28 23:42:38 -0700

    Unit tests for metrica event match moment mapper

--- tests/unit/mappings/domain/moment/test_match_moment_mapper.py
+++ tests/unit/mappings/domain/moment/test_match_moment_mapper.py
@@ -3,22 +3,24 @@ from unittest.mock import MagicMock
 from hiddenfb.domain.match import Match
 from hiddenfb.domain.moment.match import MatchMoment
 from hiddenfb.mappings.domain.moment.match import MatchMomentMapper
+from hiddenfb.schemas.data.metrica.event import MetricaEvent
 from hiddenfb.schemas.data.wyscout.event import WyscoutEvent
 from hiddenfb.test.domain.match import MatchTestUtility
 from hiddenfb.test.domain.moment.match import MatchMomentTestUtility
+from hiddenfb.test.schemas.data.metrica.event import MetricaEventTestUtility
 from hiddenfb.test.schemas.data.wyscout.event import WyscoutEventTestUtility
 
 
 def test__match_moment_mapper__creates_from_wyscout_event():
     match_utility = MatchTestUtility()
-    match: Match = match_utility.create_match(match_id=123)
+    match_: Match = match_utility.create_match(match_id=123)
 
     match_moment_utility = MatchMomentTestUtility(match_utility=match_utility)
-    match_moment: MatchMoment = match_moment_utility.create_match_moment(match=match)
+    match_moment: MatchMoment = match_moment_utility.create_match_moment(match=match_)
 
     event_utility = WyscoutEventTestUtility()
     event: WyscoutEvent = event_utility.create_event(
-        match_id=match.match_id, event_time=match_moment.frame
+        match_id=match_.match_id, event_time=match_moment.frame
     )
 
     mock_match_mapper = MagicMock()
@@ -28,3 +30,25 @@ def test__match_moment_mapper__creates_from_wyscout_event():
 
     result: MatchMoment = match_moment_mapper.from_wyscout_event(event)
     match_moment_utility.assert_equal(result, match_moment)
+
+def test__match_moment_mapper__creates_from_metrica_event():
+    match_utility = MatchTestUtility()
+    match_: Match = match_utility.create_match(match_id=123)
+
+    frame: int = 104
+    match_moment_utility = MatchMomentTestUtility(match_utility=match_utility)
+    match_moment: MatchMoment = match_moment_utility.create_match_moment(match=match_, frame=frame)
+
+    event_utility = MetricaEventTestUtility()
+    event: MetricaEvent = event_utility.create_event(
+        start_frame=frame
+    )
+
+    mock_match_mapper = MagicMock()
+    mock_match_mapper.from_metrica_event.return_value = match_moment.match
+
+    match_moment_mapper = MatchMomentMapper(match_mapper=mock_match_mapper)
+
+    result: MatchMoment = match_moment_mapper.from_metrica_event(event, match_id=match_.match_id)
+    match_moment_utility.assert_equal(result, match_moment)
+
